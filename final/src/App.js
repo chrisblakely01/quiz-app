329512e801894e59d66d2a4794cd1573371a84e0
MihaiTraista <mihai.musicmail@gmail.com>
2022-10-20 12:23:25 +0100

    changed cities

--- final/src/App.js
+++ final/src/App.js
@@ -5,8 +5,8 @@ export default function App() {
 		{
 			questionText: 'What is the capital of France?',
 			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'C1', isCorrect: false },
+				{ answerText: 'C2', isCorrect: false },
 				{ answerText: 'Paris', isCorrect: true },
 				{ answerText: 'Dublin', isCorrect: false },
 			],
